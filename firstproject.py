f646db8fc38b8847bde3c5d82555bde350980816
Angel Valdivia <angelvaldivia@Angels-MacBook-Pro.local>
2019-04-16 20:23:53 -0400

    adding my ffirst two python files.

---
+++ firstproject.py
@@ -0,0 +1,4 @@
+FIRSTNAME = input( "what is your name?")
+LASTNAME = input ("what is your last name?")
+
+print ("Hello ")
---
+++ madlibs.py
@@ -0,0 +1,6 @@
+NAME = input("Please provide a name: ")
+VERB = input("Please provide a verb: ")
+ADJECTIVE = input("Please provide an adjective: ")
+NOUN2 = input("Please provide another noun: ")
+
+print("name: ", NAME, )
