988830bc385f5e47fa094fdbdb30ff7a7dda65cc
Angel Valdivia <angelvaldivia@Angels-MBP.attlocal.net>
2019-04-22 21:20:28 -0400

    Add python day 2 exercises

---
+++ python_hello_day2.py
@@ -0,0 +1,15 @@
+# FIRSTNAME = input( "what is your name?")
+# LASTNAME = input ("what is your last name?")
+
+# print (f"Hello {FIRSTNAME}")
+
+# FIRSTNAME = input("what is your name:")
+
+# print  (f"Hello {FIRSTNAME}")
+
+# print (f"YOUR NAME HAS {len(FIRSTNAME)} LETTERS IN IT! AWESOME!")
+
+NAME = input ("What's your name: ")
+SUBJECT = input ("What's your favorite subject?: ")
+
+print (f"{NAME}'s favorite subject in school is {SUBJECT}'")
